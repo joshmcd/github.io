c40994f86cbf0ceb684d044497bd0ae7cd619ad6
Joshua McDonald <joshuadmcdonald@gmail.com>
2017-06-19 10:28:57 -0400

    edited white space

--- _plugins/video.rb
+++ _plugins/video.rb
@@ -1,38 +1,35 @@
 #this is all taken from http://www.orangenarwhals.com/2017/04/embedding-videos-in-jekyll/
 
 class Video < Liquid::Tag
-      Syntax = /^\s*([^\s]+)(\s+(\d+)\s+(\d+)\s*)?/
+    Syntax = /^\s*([^\s]+)(\s+(\d+)\s+(\d+)\s*)?/
        
-        def initialize(tagName, markup, tokens)
-                super
-                 
-                    @url = Jekyll.configuration({})['url'] || 'http://example.com'
-                     
-                        if markup =~ Syntax then
-                                  @id = $1
+    def initialize(tagName, markup, tokens)
+    super             
+      @url = Jekyll.configuration({})['url'] || 'http://example.com'                 
+        if markup =~ Syntax then
+           @id = $1
                                    
-                                        if $2.nil? then
-                                                      @width = 560
-                                                                @height = 420
-                                                                      else
-                                                                                    @width = $2.to_i
-                                                                                              @height = $3.to_i
-                                                                                                    end
-                                            else
-                                                      raise "No Video Source provided"
-                                                          end
+		if $2.nil? then
+			@width = 560
+			@height = 420
+		else
+            @width = $2.to_i
+			@height = $3.to_i
+           end
+        else
+			raise "No Video Source provided"
+           end
                          
-                          end
+        end
          
-          def lookup(context, name)
-                  lookup = context
-                      name.split(".").each { |value| lookup = lookup[value] }
-                          lookup
-                            end
+    def lookup(context, name)
+        lookup = context
+          name.split(".").each { |value| lookup = lookup[value] }
+          lookup
+    end
            
-            def render(context)
-                     "<iframe width=\"#{@width}\" height=\"#{@height}\" src=\"#{@base_url}/#{@id}\" ></iframe>"
-                       end
-             
-              Liquid::Template.register_tag "Video", self
+    def render(context)
+        "<iframe width=\"#{@width}\" height=\"#{@height}\" src=\"#{@base_url}/#{@id}\" ></iframe>"
+        end
+    Liquid::Template.register_tag "Video", self
 end
